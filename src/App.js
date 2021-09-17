09e77a48e265931dbdf9965f2c121a7c668af994
JasperHX <jasper-hendrikx@hotmail.com>
2021-09-17 23:54:33 +0200

    huiswerk

--- src/App.js
+++ src/App.js
@@ -3,9 +3,53 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Maak hier jouw formulier!
+      <>
+    <div className='container'>
+      <form>
+        <fieldset>
+          <legend>Gegevens</legend>
+          <label htmlFor='name'>
+        Naam:
+          <input
+            type='text'
+            name='name'
+            placeholder='typ hier jouw naam'
+            id='name'
+          />
+          </label>
+
+         <label htmlFor='age'>
+           Leeftijd:
+           <input
+            type='number'
+            name='age'
+            placeholder='0'
+            id='age'
+         />
+         </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Jouw Review</legend>
+          <label htmlFor='opmerkingen'>
+            Opmerkingen:
+            <input
+              type='textarea'
+              name='opmerkingen'
+              placeholder='Wat vond je van het recept?'
+              id='opmerkingen'
+
+              />
+          </label>
+
+
+
+        </fieldset>
+      </form>
+
     </div>
+
+    </>
   );
 }
 
